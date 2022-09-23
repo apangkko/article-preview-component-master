93d643225b2c1a6c9b57b3ab4b3b11168b824a35
APANG <apangkko@gmail.com>
2022-09-23 19:58:34 +0700

    v2

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const btnShare = document.querySelectorAll(".btn-share");
+const btnShare = document.querySelector(".btn-share");
+const authorShare = document.querySelector(".author-share");
 const shareBox = document.querySelector(".share-box");
 
-btnShare.forEach(btn => {
-    btn.addEventListener("click", () => {
-        btn.classList.toggle("active");
-        shareBox.classList.toggle("active");
-    })
-});
+btnShare.addEventListener("click", () => {
+  authorShare.classList.toggle("active");
+ });
 
 
 document.addEventListener("click", (event) => {
   const isClickInside = shareBox.contains(event.target);
-  const isClickInside2 = btnShare[0].contains(event.target);
+  const isClickInside2 = btnShare.contains(event.target);
 
   if (!isClickInside && !isClickInside2) {
-    shareBox.classList.remove("active");
-    btnShare[0].classList.remove("active");
+    authorShare.classList.remove("active");
   }
+
 });
